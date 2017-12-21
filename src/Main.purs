edb625b0cc2a9521aed7edd0f4606a4e86c4c857
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-12-21 15:33:21 +0100

    Show how to use typed message passing with purescript-erl-process

--- src/Main.purs
+++ src/Main.purs
@@ -3,11 +3,18 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Erl.Process (PROCESS, (!), spawn, receive)
 
 type Person = { first :: String,
                 last :: String }
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+logger = do
+    s :: String <- receive
+    log $ "Received: " <> s
+
+main :: forall e. Eff (console :: CONSOLE, process :: PROCESS | e) Unit
 main = do
   let joe = { first: "Joe", last: "Smith" }
+  p <- spawn logger
   log $ "Hello, " <> joe.first <> "!"
+  p ! "bye!"
